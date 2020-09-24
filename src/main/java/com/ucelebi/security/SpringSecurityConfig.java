be7e356c62eb8cea4e3498914855ce36f3a90573
ümit çelebi <53602829+umitcelebi@users.noreply.github.com>
2020-09-24 08:52:39 +0300

    Update SpringSecurityConfig.java

--- src/main/java/com/ucelebi/security/SpringSecurityConfig.java
+++ src/main/java/com/ucelebi/security/SpringSecurityConfig.java
@@ -28,7 +28,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/*").authenticated()
+		.antMatchers("/").authenticated()
 		.antMatchers("/home/**").hasAnyRole("ADMIN","USER")
 		.antMatchers("/book/**").hasRole("ADMIN")
 		.antMatchers("/author/**").hasRole("ADMIN")
