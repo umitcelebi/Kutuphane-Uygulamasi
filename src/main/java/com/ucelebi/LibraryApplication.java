dd4196e48f46cf65a0b6980ce6467eaaace944d1
ümit çelebi <53602829+umitcelebi@users.noreply.github.com>
2020-09-25 10:07:37 +0300

    Update LibraryApplication.java

--- src/main/java/com/ucelebi/LibraryApplication.java
+++ src/main/java/com/ucelebi/LibraryApplication.java
@@ -1,12 +1,11 @@
 package com.ucelebi;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.ucelebi.repo.UserRepository;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
