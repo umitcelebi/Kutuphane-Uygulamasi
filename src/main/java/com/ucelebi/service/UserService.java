65f83ecddeaa3f463e7982857297a2c9f56984e2
ümit çelebi <53602829+umitcelebi@users.noreply.github.com>
2020-09-28 12:45:54 +0300

    Update UserService.java

--- src/main/java/com/ucelebi/service/UserService.java
+++ src/main/java/com/ucelebi/service/UserService.java
@@ -1,7 +1,9 @@
 package com.ucelebi.service;
 
+import java.util.Optional;
 import com.ucelebi.models.User;
 
 public interface UserService {
 	boolean signup(User user);
+	Optional<User> findByUsername(String username);
 }
